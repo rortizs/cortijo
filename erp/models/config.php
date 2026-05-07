4c829c408ff2dbe271d964815c7e40a26a359366
Richard David Ortiz Sasvin <37150696+rortizs@users.noreply.github.com>
2026-05-07 15:20:13 -0600

    security: remove hardcoded DB credentials from config.php

--- erp/models/config.php
+++ erp/models/config.php
@@ -5,22 +5,20 @@
  *
  * @author Richard Sasvin
  */
-class Config {
+class Config
+{
 
-    public static $systemName = "ERP - DIGICOM.COM.GT";
-    public static $dbD = "information_schema";
-	public static $host = "45.79.3.109";
-    //public static $host = "192.168.1.19";
-    public static $userDB = "root";
-    //public static $pwdDB = "R!c@rd0#2020.1"; esta clave ya no funciona
-    public static $pwdDB = "10Br3nd!t@#102022";    //clave correcta
-    //public static $pwdDB = "c0rt23062022"; //clave local
+  public static $systemName = "ERP - DIGICOM.COM.GT";
+  public static $dbD = "information_schema";
+  public static $host = "45.79.3.109";
+  public static $userDB = "root";
+  public static $pwdDB = "";    // credencial movida fuera del repo — leer de variable de entorno o archivo externo
 
-    public static function redirectTohttps() {
-        if ($_SERVER['HTTPS'] != "on") {
-            $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            header("Location:$redirect");
-        }
+  public static function redirectTohttps()
+  {
+    if ($_SERVER['HTTPS'] != "on") {
+      $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+      header("Location:$redirect");
     }
-
+  }
 }
