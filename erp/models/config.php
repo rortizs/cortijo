8b7ac4b31658cae6806ba2423252b1ebe510dff7
Richard David Ortiz Sasvin <37150696+rortizs@users.noreply.github.com>
2026-05-07 15:31:09 -0600

    security: load DB credentials from external file outside webroot

--- erp/models/config.php
+++ erp/models/config.php
@@ -5,14 +5,20 @@
  *
  * @author Richard Sasvin
  */
+
+// Credenciales cargadas desde archivo externo fuera del webroot
+if (file_exists('/etc/erp-cortijo.conf.php')) {
+  require_once '/etc/erp-cortijo.conf.php';
+}
+
 class Config
 {
 
   public static $systemName = "ERP - DIGICOM.COM.GT";
   public static $dbD = "information_schema";
-  public static $host = "45.79.3.109";
-  public static $userDB = "root";
-  public static $pwdDB = "";    // credencial movida fuera del repo — leer de variable de entorno o archivo externo
+  public static $host = defined('DB_HOST') ? DB_HOST : '127.0.0.1';
+  public static $userDB = defined('DB_USER') ? DB_USER : '';
+  public static $pwdDB = defined('DB_PASS') ? DB_PASS : '';
 
   public static function redirectTohttps()
   {
